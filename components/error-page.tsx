1751a673983fa267799bfb07203a5bef099b615f
Djonathan Krause <email@djonathan.com>
2025-04-18 15:30:23 +0200

    x

--- components/error-page.tsx
+++ components/error-page.tsx
@@ -1,12 +1,12 @@
+"use client";
+
 export default function ErrorPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="text-center">
-        <h1 className="text-3xl font-bold text-red-500 mb-4">
-          Authentication Error
-        </h1>
+        <h1 className="text-3xl font-bold text-red-500 mb-4">Error</h1>
         <p className="text-gray-300 mb-6">
-          Oops! Something went wrong during the login process. Please try again.
+          Something went wrong. Please try again later.
         </p>
         <button
           className="spotify-button"
